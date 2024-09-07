55b745b3b9478b8238260ea99b1c6f05d514297e
nodzsound <22749441+nodzsound@users.noreply.github.com>
2024-09-07 20:46:06 +0200

    Added GlueRip Meta Header with Tag noindex to prevent premature indexing

---
+++ GlueRip/GlueRip.lua
@@ -0,0 +1,17 @@
+@noindex
+@description GlueRip
+@version 0.0.1
+@author nodzsound
+@metapackage
+@links 
+     Website https://nico-dilz.com
+     Instagram https://www.instagram.com/nodzsound/
+     LinkedIn https://www.linkedin.com/in/nodzsound/    
+@about
+ This package offers you the possibility to unglue previously glued items. 
+ I hope that you find this package helpful. To report any bugs, please mail to: 
+ reapack@nico-dilz.com 
+@provides
+
+@changelog 
+     Dev Version
