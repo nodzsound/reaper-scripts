c157b64ae469a5677e341ef0142e0b9c920c8ff3
nodzsound <22749441+nodzsound@users.noreply.github.com>
2024-09-07 17:09:03 +0200

    Updated the Meta Header

--- Lock Tracks/Lock Tracks.lua
+++ Lock Tracks/Lock Tracks.lua
@@ -1,5 +1,5 @@
 @description Lock Tracks
-@version 0.0.1
+@version 1.0.0
 @author nodzsound
 @links 
      Website https://nico-dilz.com
@@ -12,5 +12,7 @@
  reapack@nico-dilz.com 
 @provides
     [main] nodzsound-Toggle_Track_Lock.lua
+    [main] nodzsound-Track_Lock_Off.lua
+    [main] nodzsound-Track_Lock_On.lua
 @changelog 
-     Set up the meta description
+     First Version of this script package
